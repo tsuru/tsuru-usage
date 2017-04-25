64629adb44b384fc6ea251d1922c7595781d9dfe
André Carvalho <asantostc@gmail.com>
2017-04-25 10:57:11 -0300

    log api errors

--- api/api.go
+++ api/api.go
@@ -5,6 +5,7 @@
 package api
 
 import (
+	"log"
 	"net/http"
 	"strings"
 
@@ -15,11 +16,12 @@ import (
 type handler func(http.ResponseWriter, *http.Request) error
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	handleError(h(w, r), w)
+	handleError(h(w, r), w, r)
 }
 
-func handleError(err error, w http.ResponseWriter) {
+func handleError(err error, w http.ResponseWriter, r *http.Request) {
 	if err != nil {
+		log.Printf("[api] %s: %s", r.URL.Path, err.Error())
 		if strings.Contains(err.Error(), "not found") {
 			http.Error(w, err.Error(), http.StatusNotFound)
 		} else {
@@ -38,9 +40,9 @@ func Router(m *mux.Router, tsuruAPI tsuru.TsuruAPI) {
 	m.Handle("/plans/cost", handler(updatePlanCost)).Methods(http.MethodPut)
 	m.Handle("/plans/cost", handler(listPlanCosts))
 	m.HandleFunc("/teams", func(w http.ResponseWriter, r *http.Request) {
-		handleError(listTeams(w, r, tsuruAPI), w)
+		handleError(listTeams(w, r, tsuruAPI), w, r)
 	})
 	m.HandleFunc("/pools", func(w http.ResponseWriter, r *http.Request) {
-		handleError(listPools(w, r, tsuruAPI), w)
+		handleError(listPools(w, r, tsuruAPI), w, r)
 	})
 }
