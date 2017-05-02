b1742cc5703657710ced886a006ede8b2f8be7cc
Guilherme Garnier <guilherme.garnier@gmail.com>
2017-05-02 15:12:33 -0300

    admin: add basic http auth

--- admin/admin.go
+++ admin/admin.go
@@ -5,7 +5,9 @@
 package admin
 
 import (
+	"crypto/subtle"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/gorilla/mux"
@@ -29,9 +31,27 @@ func Router(m *mux.Router) {
 	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/admin/teamgroups", 301)
 	})
-	m.HandleFunc("/teamgroups", groupListHandler)
-	m.HandleFunc("/teamgroups/new", groupNewHandler).Methods(http.MethodGet)
-	m.HandleFunc("/teamgroups/{name}", groupEditHandler).Methods(http.MethodGet)
-	m.HandleFunc("/teamgroups/{name}", groupUpdateHandler).Methods(http.MethodPost)
-	m.HandleFunc("/teamgroups/{name}", groupDeleteHandler).Methods(http.MethodDelete)
+	m.HandleFunc("/teamgroups", basicAuth(groupListHandler))
+	m.HandleFunc("/teamgroups/new", basicAuth(groupNewHandler)).Methods(http.MethodGet)
+	m.HandleFunc("/teamgroups/{name}", basicAuth(groupEditHandler)).Methods(http.MethodGet)
+	m.HandleFunc("/teamgroups/{name}", basicAuth(groupUpdateHandler)).Methods(http.MethodPost)
+	m.HandleFunc("/teamgroups/{name}", basicAuth(groupDeleteHandler)).Methods(http.MethodDelete)
+}
+
+func basicAuth(handler http.HandlerFunc) http.HandlerFunc {
+	username := os.Getenv("ADMIN_USERNAME")
+	password := os.Getenv("ADMIN_PASSWORD")
+	if len(username) == 0 || len(password) == 0 {
+		return handler
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		user, pass, ok := r.BasicAuth()
+		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(password)) != 1 {
+			w.Header().Set("WWW-Authenticate", `Basic realm="password"`)
+			w.WriteHeader(401)
+			w.Write([]byte("Unauthorized.\n"))
+			return
+		}
+		handler(w, r)
+	}
 }
